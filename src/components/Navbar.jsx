fc41ba46bd7cbf5beeb0d9319e5e37dabdb437f8
novachan10 <novac8167@gmail.com>
2023-03-28 10:28:45 +0700

    update navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,79 @@
+import { useState } from "react";
 
 const Navbar = () => {
-  
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "News", link: "/#News" },
+    { name: "Contact", link: "/#Footer" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <></>
+    <div className="shadow-md w-full fixed top-0 left-0 bg-white">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7 container mx-auto">
+        <div
+          className="font-bold text-xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800 "
+        >
+          <h2>
+            Huang <span className="text-blue-500">News</span>
+          </h2>
+        </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden"
+        >
+          {open ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-7 transition-all duration-500 ease-in ${
+            open ? "top-10 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-blue-400 duration-500 text-base"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
