0ba8717d16c6ecab22158e19768fca460586a091
Nayla Nur Amalia <95407345+nylaam@users.noreply.github.com>
2023-03-28 10:55:35 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 const Navbar = () => {
   let Links = [
-    { name: "Home", link: "/" },
-    { name: "News", link: "/#News" },
-    { name: "Contact", link: "/#Footer" },
+    { name: "Beranda", link: "/" },
+    { name: "Berita", link: "/#News" },
+    { name: "Hubungi", link: "/#Footer" },
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className="shadow-md w-full fixed top-0 left-0 bg-white">
+    <div className="shadow-md w-full fixed top-0 left-0 bg-white z-[999]">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7 container mx-auto">
         <div
           className="font-bold text-xl cursor-pointer flex items-center font-[Poppins] 
